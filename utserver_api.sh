b6142bfe4a3f778164dd788dd8fb234b5b881a03
Niki <nhsqr.mail@gmail.com>
2017-03-20 16:34:20 +0200

    Added more options
    Added option for get and set. Some functions are rewritten. Unnecessary lines removed.

--- utserver_api.sh
+++ utserver_api.sh
@@ -13,18 +13,28 @@ TEMPFILE="/tmp/utserver_api.tmp"
 TOKEN=`${CURL} -u ${USERNAME}:${PASSWORD} -G -s -c cookie.txt -n ${UTORRENTURL}token.html | sed 's/<[^<>]*>//g'`
 ${CURL} -u ${USERNAME}:${PASSWORD} -G -s -n -b cookie.txt -d token=${TOKEN} -d list=1 ${UTORRENTURL} > ${TEMPFILE}
 
+#Check if all parameters are supplied
+if [ $1 != "--help" ]
+then
+	if [ -z $2 ] || [ -z $3 ]
+	then
+		echo "Not all parameters are supplied. Try: $(basename $0) --help"
+		rm ${TEMPFILE} cookie.txt
+		exit 0
+	fi
+fi
+
 #Third parameter is always INDEX
 index=$3
 
 #gethash index (return hash)
 function gethash {
-	getoption 1 $1 ret
-	eval "$2=$ret"
+	echo `getoption 1 $1`
 }
 
 #getoption option index (return string | integer)
 function getoption {
-	eval "$3=`cat ${TEMPFILE} | awk -F',' 'NF > 20' | grep -Po '(?<=\[).*(?=\])' | cut -d',' -f$1 | awk "NR==$2"`"
+	echo `cat ${TEMPFILE} | awk -F',' 'NF > 20' | grep -Po '(?<=\[).*(?=\])' | cut -d',' -f$1 | awk "NR==$2"`
 }
 
 function usage {
@@ -59,6 +69,16 @@ function usage {
 	echo -e "\tremaining	- remaining size in bytes"
 	echo -e "\tstatus		- current status of the torrent"
 	echo -e "\tfolder		- folder of downloaded torrent"
+	echo -e "\ttrackers	- list of trackers"
+	echo -e "\tulrate		- upload rate (integer in bytes per second)"
+	echo -e "\tdlrate		- download rate (integer in bytes per second)"
+	echo -e "\tsuperseed	- superseed (integer: -1 = Not allowed; 0 = Disabled; 1 = Enabled)"
+	echo -e "\tdht		- use DHT (integer: -1 = Not allowed; 0 = Disabled; 1 = Enabled)"
+	echo -e "\tpex		- use peer exchange (integer: -1 = Not allowed; 0 = Disabled; 1 = Enabled)"
+	echo -e "\tseed_override	- override seed queueing (integer: -1 = Not allowed; 0 = Disabled; 1 = Enabled)"
+	echo -e "\tseed_ratio	- seed ratio before stopping the torrent (integer in per mils; 0 = ignore)"
+	echo -e "\tseed_time	- seed time in seconds before stopping the torrent; 0 = ignore"
+	echo -e "\tulslots		- upload slots (integer)"
 	echo -e "\nOPTION2:"
 	echo -e "\tstart 		- start torrent"
 	echo -e "\tstop		- stop torrent"
@@ -72,6 +92,14 @@ function usage {
 	echo -e "\tqueuedown	- move torrent one queue position down"
 	echo -e "\tqueuetop	- move torrent to the top of the queue"
 	echo -e "\tqueueup		- move torrent one queue position up"
+	echo -e "\tlabel=LABEL		- set torrent label to value LABEL"
+	echo -e "\tulrate=INTEGER		- set upload rate to value INTEGER (integer in bytes per second)"
+	echo -e "\tdlrate=INTEGER		- set download rate to value INTEGER (integer in bytes per second)"
+	echo -e "\tsuperseed=INTEGER	- set superseed to value INTEGER (integer: -1 = Not allowed; 0 = Disabled; 1 = Enabled)"
+	echo -e "\tseed_override=INTEGER	- set override seed queueing to value INTEGER (integer: -1 = Not allowed; 0 = Disabled; 1 = Enabled)"
+	echo -e "\tseed_ratio=INTEGER	- set seed ratio before stopping the torrent to value INTEGER (integer in per mils; 0 = ignore)"
+	echo -e "\tseed_time=INTEGER	- set seed time in seconds before stopping the torrent to value INTEGER; 0 = ignore"
+	echo -e "\tulslots=INTEGER		- set upload slots to value INTEGER (integer)"
 }
 case "$1" in
 --get | -g )
@@ -139,8 +167,16 @@ case "$1" in
 	"folder")
 	    option="27"
 		;;
+	"trackers" | "ulrate" | "dlrate" | "superseed" | "dht" | "pex" | "seed_override" | "seed_ratio" | "seed_time" | "ulslots" | "seed_num")
+		torrenthash=`gethash $index | cut -d'"' -f2`
+		props=`${CURL} -u ${USERNAME}:${PASSWORD} -G -s -n -b cookie.txt -d token=${TOKEN} -d action=getprops -d hash=$torrenthash ${UTORRENTURL}`
+		echo -e `cat getprops | grep "$2" | awk -F': ' '{print $2}' | cut -d'"' -f2`
+		rm ${TEMPFILE} cookie.txt
+		exit 0
+		;;
 	*)
 		usage
+		rm ${TEMPFILE} cookie.txt
 		exit 0
 		;;
 	esac
@@ -153,31 +189,38 @@ case "$1" in
 		exit 0
 	elif [ "$index" = "all" ]
 	then
-		#cat ${TEMPFILE} | head -n -6 | tail -n +3 | tr -cd '[:alnum:]''[=,=]''[=.=]''[=-=]''[:space:]' | cut -d',' -f3 | nl
-		#cat ${TEMPFILE} | awk -F[\[] '{print $2}' | awk -F[\]] '{print $1$2}' | awk -F',' 'NF > 20' | cut -d',' -f3 | nl
-		#cat ${TEMPFILE} | awk -F[\[] '{if  ($3 == "") print $2; else print $2"["$3;}' | awk -F[\]] '{if ($2 == "," || $2 == "") print $1; else print $1"]"$2}' | awk -F',' 'NF > 20' | cut -d',' -f3 | nl
-		#cat ${TEMPFILE} | awk -F',' 'NF > 20' | grep -Po '(?<=\[).*(?=\])' | cut -d',' -f3 | nl
 		cat ${TEMPFILE} | awk -F',' 'NF > 20' | grep -Po '(?<=\[).*(?=\])' | cut -d',' -f$option | nl
 	else
-		getoption $option $index ret
-		echo "$ret"
-		#cat ${TEMPFILE} | awk -F',' 'NF > 20' | grep -Po '(?<=\[).*(?=\])' | cut -d',' -f$option | awk "NR==$index"
-		#cat ${TEMPFILE} | awk -F[\[] '{if  ($3 == "") print $2; else print $2"["$3;}' | awk -F[\]] '{if ($2 == "," || $2 == "") print $1; else print $1"]"$2}' | awk -F',' 'NF > 20' | cut -d',' -f$option | awk "NR==$index"
-		#cat ${TEMPFILE} | awk -F[\[] '{print $2}' | awk -F[\]] '{print $1$2}' | awk -F',' 'NF > 20' | cut -d',' -f$option | awk "NR==$index"
-		#cat ${TEMPFILE} | head -n -5 | tail -n +3 | awk "NR==$index" | cut -d'[' -f2 | cut -d']' -f1 | cut -d',' -f$option
-		#cat ${TEMPFILE} | head -n -5 | tail -n +3 | tr -cd '[:alnum:]''[=,=]''[=.=]''[=-=]''[:space:]' | cut -d',' -f$option | awk "NR==$index"
+		echo `getoption $option $index`
 	fi
 	;;
 --set | -s )
-	gethash $index torrenthash
-	${CURL} -u ${USERNAME}:${PASSWORD} -G -s -n -b cookie.txt -d token=${TOKEN} -d action=$2 -d hash=$torrenthash ${UTORRENTURL} > /dev/null
-	if [ $? -gt 0 ]
-	then
-		echo "Error sending request to server. Check settings."
-	else
-		getoption 3 $index ret
-		echo "Torrent $2 success: $ret"
-	fi
+	property=`echo $2 | cut -d'=' -f1`
+	value=`echo $2 | cut -d'=' -f2`
+	torrenthash=`gethash $index | cut -d'"' -f2`
+	case "$property" in
+	"start" | "stop" | "pause" | "unpause" | "forcestart" | "recheck" | "remove" | "removedata" | "queuebottom" | "queuedown" | "queuetop" | "queueup" )
+		${CURL} -u ${USERNAME}:${PASSWORD} -G -s -n -b cookie.txt -d token=${TOKEN} -d action=$2 -d hash=$torrenthash ${UTORRENTURL} > /dev/null
+		if [ $? -gt 0 ]
+		then
+			echo "Error sending request to server. Check settings."
+		else
+			echo "Torrent $2 success: `getoption 3 $index`"
+		fi
+		;;
+	"label" | "ulrate" | "dlrate" | "superseed" | "seed_override" | "seed_ratio" | "seed_time" | "ulslots" )
+		${CURL} -u ${USERNAME}:${PASSWORD} -G -s -n -b cookie.txt -d token=${TOKEN} -d action=setprops -d hash=$torrenthash -d s=$property -d v=$value ${UTORRENTURL} > /dev/null
+		if [ $? -gt 0 ]
+		then
+			echo "Error ($?) sending request to server. Check settings."
+		else
+			echo "Torrent setting $property=$value success: `getoption 3 $index`"
+		fi
+		;;
+	* )
+		echo -e "Invalid request: $property\nTry: $(basename $0) --help"
+		;;
+	esac
 	;;
 --help | -h )
 	usage
